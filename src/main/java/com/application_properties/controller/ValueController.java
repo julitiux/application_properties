2bdc452e4c025193694aecac62dc7f5481565a63
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-30 02:51:35 -0600

    add tags @RestController and @Value for get the value from application.properties

--- src/main/java/com/application_properties/controller/ValueController.java
+++ src/main/java/com/application_properties/controller/ValueController.java
@@ -1,4 +1,12 @@
 package com.application_properties.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ValueController {
+
+  @Value("${spring.application.name}")
+  private String nameApplication;
+
 }
