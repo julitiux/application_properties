c910fe1596ec1e5fb7d207b0e492f0b21a7f15db
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-30 02:53:46 -0600

    implement method getApplicationName using @ResponseBody just for return a HTML with the name of application name

--- src/main/java/com/application_properties/controller/ValueController.java
+++ src/main/java/com/application_properties/controller/ValueController.java
@@ -1,6 +1,9 @@
 package com.application_properties.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +11,10 @@ public class ValueController {
 
   @Value("${spring.application.name}")
   private String nameApplication;
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public @ResponseBody String getApplicationName() {
+    return nameApplication;
+  }
 
 }
